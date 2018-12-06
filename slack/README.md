67bb0d665b19fe84a5efec80a55d02a96e3bb51d
Tsukada <seiya.tsukada@accenture.com>
2018-12-06 19:37:58 +0900

    commit

--- slack/README.md
+++ slack/README.md
@@ -1,1 +1,26 @@
 # python_lambda
+
+## Post Slack
+
+pip install
+
+```
+pip install -r requirements.txt -t .
+```
+
+to zip
+
+```
+zip -r /tmp/output.zip ./*
+```
+
+zip upload to lambda
+
+```
+aws lambda \
+update-function-code \
+--profile <aws_profile> \
+--function-name <function_name> \
+--zip-file fileb:///tmp/output.zip \
+--publish
+```
--- slack/lambda_to_slack.py
+++ slack/lambda_to_slack.py
@@ -4,43 +4,45 @@ import requests
 import json
 import random
 
-year_list = ["28", "29", "30"]
-half_list = ["1", "2"]
-question_list = list()
-slack_url = ""
+def lambda_handler(event, context):
 
-# site_url_base = "https://www.nw-siken.com/kakomon/30_aki/am1_2.html"
+    year_list = ["28", "29", "30"]
+    half_list = ["1", "2"]
+    question_list = list()
+    slack_url = os.environ.get("SLACK_URL")
 
-site_url_base = "https://www.nw-siken.com/kakomon/"
-year = random.choice(year_list)
-half = random.choice(half_list)
+    # site_url_base = "https://www.nw-siken.com/kakomon/30_aki/am1_2.html"
 
-if half == "1":
-    question_list = range(1, 31)
-    question = random.choice(question_list)
-elif half == "2":
-    question_list = range(1, 26)
-    question = random.choice(question_list)
+    site_url_base = "https://www.nw-siken.com/kakomon/"
+    year = random.choice(year_list)
+    half = random.choice(half_list)
 
-suffix_url = "{0}_aki/am{1}_{2}.html".format(year, half, question)
+    if half == "1":
+        question_list = range(1, 31)
+        question = random.choice(question_list)
+    elif half == "2":
+        question_list = range(1, 26)
+        question = random.choice(question_list)
 
-site_url = site_url_base + suffix_url
+    suffix_url = "{0}_aki/am{1}_{2}.html".format(year, half, question)
 
-print site_url
+    site_url = site_url_base + suffix_url
 
-# create content
-content = """
+    print site_url
 
-今日の問題はこれです
+    # create content
+    content = """
 
-{0}
+    今日の問題はこれです
 
-""".format(site_url)
+    {0}
 
-# post to slack
-requests.post(slack_url, data = json.dumps({
-    "text": content,
-    "username": "my_bot",
-    "icon_emoji": ":ghost:",
-    "link_names": 1,
-}))
+    """.format(site_url)
+
+    # post to slack
+    requests.post(slack_url, data = json.dumps({
+        "text": content,
+        "username": "my_bot",
+        "icon_emoji": ":ghost:",
+        "link_names": 1,
+    }))
